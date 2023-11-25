f1c19df221a84cf540a7359adb586539566d6c45
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-11-25 17:26:41 -0300

    scrapping from first source

--- src/world_uni_api/mix.exs
+++ src/world_uni_api/mix.exs
@@ -24,6 +24,8 @@ defmodule WorldUniApi.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:crawly, "~> 0.16.0"},
+      {:floki, "~> 0.33.0"}
     ]
   end
 end
